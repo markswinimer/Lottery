30c1702d46c49ca18db8635f3175da9a0c52b140
markswinimer <markswinimer@gmail.com>
2019-09-10 21:57:51 -0400

    initial commit, removes logo

---
+++ src/Lottery.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Lottery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // STATE HERE
+    }
+  }
+  render() {
+    return(
+      <div>
+        <h1>Lottery</h1>
+      </div>
+    )
+  }
+}
+export default Lottery;
